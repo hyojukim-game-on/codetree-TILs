2a63de16b7e5be51e5bf64b120ba2dd1465915c4
HYOJU KIM <139435373+hyojukim-game-on@users.noreply.github.com>
2024-04-15 18:00:00 +0900

    Title: a 부터 b 까지, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240415/a 부터 b 까지/a-to-b.java
+++ 240415/a 부터 b 까지/a-to-b.java
@@ -4,20 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // 변수 선언
-        int a;
-        int b;
+        int a, b, i;
         
         // 입력
         a = sc.nextInt();
         b = sc.nextInt();
+        i = a;
 
         // 출력
-        for ( int i = a; i <= b; ) {
-            if ( i % 2 == 1 ) {
-                System.out.print(i+" ");
+        while(i <= b) {
+            System.out.print(i + " ");
+            if (i % 2 == 1) {
                 i *= 2;
             } else {
-                System.out.print(i+" ");
                 i += 3;
             }
         }
