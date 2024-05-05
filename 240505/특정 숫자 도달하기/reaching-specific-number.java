c5583e14e359225ba4804d3747006b5d776fc412
HYOJU KIM <139435373+hyojukim-game-on@users.noreply.github.com>
2024-05-05 12:07:35 +0900

    Title: 특정 숫자 도달하기, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240505/특정 숫자 도달하기/reaching-specific-number.java
+++ 240505/특정 숫자 도달하기/reaching-specific-number.java
@@ -6,27 +6,32 @@ public class Main {
         int[] arr = new int[10];
         int sumValue = 0;
         int cnt = 0;
+        double avgValue = 0;
         int firstNum = sc.nextInt();
         arr[0] = firstNum;
-        for (int i = 1; i < 10; i++) {
+        for (int i=1; i<10; i++) {
             int num = sc.nextInt();
-            if (num < 250) {
+            if ( num < 250 ) {
                 arr[i] = num;
             } else {
                 break;
             }
         }
-        for (int j = 0; j < 10; j++) {
+        for (int j=0; j<10; j++) {
+            // System.out.println("j:"+j);
+            // System.out.println("sumValue:"+sumValue);
+            // System.out.println("cnt:"+cnt);
+            // System.out.println("arr[j]:"+arr[j]);
             if (arr[j] != 0) {
                 sumValue += arr[j];
                 cnt += 1;
-            } else {
-                break;
-            }
+            } else { 
+                break; 
+            };
         }
-
-        double avgValue = sumValue / (double) cnt;
-
-        System.out.printf("%d %.1f", sumValue, avgValue);
+        avgValue += (sumValue / (double) cnt);
+        System.out.printf("%d", sumValue);
+        System.out.print(" ");
+        System.out.printf("%.1f", avgValue);
     }
 }
