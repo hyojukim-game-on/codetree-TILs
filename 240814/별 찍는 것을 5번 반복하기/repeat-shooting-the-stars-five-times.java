4fcbc4986ac87b251e0502a14adc73b4312ab412
HYOJU KIM <139435373+hyojukim-game-on@users.noreply.github.com>
2024-08-14 16:23:35 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240814/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+    public static void print10stars() {
+        System.out.println("**********");
+    }
+    
+    public static void main(String[] args) {
+        for (int i=0; i<5; i++) {
+            print10stars();
+        }
+    }
+}
